5e8fcc688f34a58ba6d50a600e762c21ab1d3c17
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-02 07:42:58 +0000

    Done

--- build.py
+++ build.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 def solution(array):
-    """
-    Enter your code here
-    """
+    array1 = np.array(array)
+    newshape = 1
+    for i in array1.shape:
+        newshape *= i
+    array1 = np.reshape(array1, newshape)
+    return np.array(list(set(array1)))
