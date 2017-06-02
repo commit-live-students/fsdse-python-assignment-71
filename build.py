7e3e02468d3e6390d4b4fa003a61caf72cfacdf9
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-02 07:56:40 +0000

    Done

--- build.py
+++ build.py
@@ -2,9 +2,4 @@ import numpy as np
 
 
 def solution(array):
-    array1 = np.array(array)
-    newshape = 1
-    for i in array1.shape:
-        newshape *= i
-    array1 = np.reshape(array1, newshape)
-    return np.array(list(set(array1)))
+    return np.unique(array)
