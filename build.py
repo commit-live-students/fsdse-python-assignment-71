064392dc42a97780759c6b10c16d240fe27c1157
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-03 06:23:32 +0000

    Done

--- build.py
+++ build.py
@@ -5,3 +5,9 @@ def solution(array):
     """
     Enter your code here
     """
+    array = np.array(array)
+    uniqueArray = np.unique(array)
+
+    return uniqueArray
+
+solution([10,10,20,20,30 ,30])
