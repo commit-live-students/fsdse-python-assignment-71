688030421e316c752ba4a52a65b8ef32c1fc2e43
harishkandan <harishkandan95@gmail.com>
2017-06-19 18:11:47 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,19 @@
 import numpy as np
 
-
 def solution(array):
-    """
-    Enter your code here
-    """
+    array=np.array(array)
+    if array.ndim==1:
+        ans=[]
+        for i in array:
+            if i not in ans:
+                ans.append(i)
+        return ans
+    if array.ndim==2:
+        ans=[]
+        for i in array[0]:
+            if i not in ans:
+                ans.append(i)
+        for i in array[1]:
+            if i not in ans:
+                ans.append(i)
+        return ans
