9e6b71a4b09f346b582ae7e10f2319afbb43a6f3
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-19 11:10:29 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
+"""
+Write a Python program to get the unique elements of an array using Numpy.
+Sample Output:
+Scenario 1:
+
+Original array: [10 10 20 20 30 30]
+Unique elements of the above array: [10 20 30]
+Scenario 2:
+
+Original array: [[1 1] [2 3]]
+Unique elements of the above array: [1 2 3]
+"""
 import numpy as np
-
-
 def solution(array):
-    """
-    Enter your code here
-    """
+    return np.unique(array)
